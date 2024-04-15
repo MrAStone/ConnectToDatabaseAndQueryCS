18736ac828af306e54c77e565f13e2459dfc4d57
Al Stone <astone@heckgrammar.co.uk>
2024-04-15 11:49:22 +0100

    Simple Count Query - Execute Scalar

--- ConnectToDatabaseAndQueryCS/Program.cs
+++ ConnectToDatabaseAndQueryCS/Program.cs
@@ -7,6 +7,15 @@ namespace ConnectToDatabaseAndQueryCS
         {
             string serverDetails = "server=172.16.11.131;uid=astone;pwd=BPmXzkau;database=astone_test";
             MySqlConnection con = new MySqlConnection(serverDetails);
+            MySqlCommand cmd = new MySqlCommand();
+            //Simple count query - returns 1 result
+            String SQL = "SELECT COUNT(SoftwareID) FROM SoftwareLicence";
+            cmd.CommandText = SQL;
+            cmd.Connection = con;
+            con.Open(); // open connection
+            int result = Convert.ToInt32(cmd.ExecuteScalar()); // result is known to be integer
+            Console.WriteLine(result);
+            con.Close(); // close connection
 
         }
     }
